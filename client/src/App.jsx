98f6fe38e0ec9875d5f148fa761ac139a185e7bd
Abhisheksingh10022 <84452461+Abhisheksingh10022@users.noreply.github.com>
2022-01-23 10:10:22 +0530

    add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,18 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./Screens/Homescree";
+import {Route,Routes} from"react-router-dom";
+import ProductScreen from "./Screens/ProductScreen";
 const App=()=> {
   return (
    <>
    <Header />
    <main className="py-3">
      <Container>
-<HomeScreen />
+       <Routes>
+<Route path='/' element={<HomeScreen/>} exact></Route>
+<Route path='/product/:_id' element={<ProductScreen></ProductScreen>} exact />
+</Routes>
    </Container>
    </main>
    <Footer />
